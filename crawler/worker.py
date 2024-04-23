2563d468b670d356e68a299a5e109825f96ced93
Zhenglin Ning <zhenglin@circinus-7.ics.uci.edu>
2024-04-22 20:08:10 -0700

    update

--- crawler/worker.py
+++ crawler/worker.py
@@ -5,6 +5,9 @@ from utils.download import download
 from utils import get_logger
 import scraper
 import time
+from urllib.parse import urljoin, urlparse
+from urllib.robotparser import RobotFileParser
+import socket
 
 
 class Worker(Thread):
@@ -23,12 +26,42 @@ class Worker(Thread):
             if not tbd_url:
                 self.logger.info("Frontier is empty. Stopping Crawler.")
                 break
+            parsed_url = urlparse(tbd_url)
+            base_url = parsed_url.scheme + "://" + parsed_url.netloc
+            if not self.robot_allowed(base_url):
+                self.logger.info(f"Skipping {tbd_url} due to robots.txt rules")
+                continue
             resp = download(tbd_url, self.config, self.logger)
             self.logger.info(
-                f"Downloaded {tbd_url}, status <{resp.status}>, "
+                f"Downloaded {tbd_url}, status <{resp.status}>., "
                 f"using cache {self.config.cache_server}.")
             scraped_urls = scraper.scraper(tbd_url, resp)
+            # print("David test")
+            # print(scraped_urls)
             for scraped_url in scraped_urls:
                 self.frontier.add_url(scraped_url)
             self.frontier.mark_url_complete(tbd_url)
             time.sleep(self.config.time_delay)
+
+    def fetch_robots(self, url):
+        parser = RobotFileParser()
+        robots_url = urljoin(url, '/robots.txt')
+        try:
+            socket.setdefaulttimeout(3)
+            parser.set_url(robots_url)
+            parser.read()
+            return parser
+        except socket.timeout:
+            self.logger.warning("Timeout occurred while fetching robots.txt")
+            return None
+        except Exception as e:
+            self.logger.warning(f"Failed to fetch or parse robots.txt for {url}: {e}")
+            return None
+        finally:
+            socket.setdefaulttimeout(None)
+
+    def robot_allowed(self, url):
+        robot = self.fetch_robots(url)
+        if robot:
+            return robot.can_fetch("*", url)
+        return True
--- scraper.py
+++ scraper.py
@@ -1,5 +1,5 @@
 import re
-from urllib.parse import urlparse
+from urllib.parse import urlparse, urljoin
 from bs4 import BeautifulSoup
 
 def scraper(url, resp):
@@ -21,18 +21,51 @@ def extract_next_links(url, resp):
     if resp.status != 200:
         print(resp.error)
         return list()
+    elif not resp.raw_response.content:
+        return list()
     
     # decode bytes to html
-    decoded = resp.raw_response.content.decode("utf-8")
+    decoded = resp.raw_response.content.decode("utf-8", errors="ignore")
     
     # parse for href
     soup = BeautifulSoup(decoded, 'html.parser')
+
+    # get base url in case of relative urls found
+    base_url = resp.url
+
+    # FILTER OUT: large & small files
+    text = soup.get_text()
+    text_length = len(text)
+    # print("text length: ", text_length)
+    # TODO: check for outlying large sizes & small sizes
+    with open("length_threshold.txt", "a") as file:
+        file.write(str(text_length)+"\n")
+
+    # FILTER OUT: low information
+    html_length = len(soup.prettify())
+    ratio = text_length / html_length
+    # print("ratio: ", ratio)
+    # TODO: check for low ratios
+    with open("ratio_threshold.txt", "a") as file:
+        file.write(str(ratio)+"\n")
+
+    # FILTER OUT: similar pages w/ simhashing
+    
+
+
     final_links = []
     links = soup.find_all("a")
     for link in links:
         href = link.get("href")
         if href != "#" and href is not None:
-            final_links.append(href)
+            absolute_link = href
+            # check if the link is already an absolute URL
+            if not urlparse(href).scheme:
+                # convert relative link to absolute link
+                absolute_link = urljoin(base_url, href)
+            final_links.append(absolute_link)
+    # add into the buffer
+    
     return final_links
 
 def is_valid(url):
@@ -40,12 +73,20 @@ def is_valid(url):
     # If you decide to crawl it, return True; otherwise return False.
     # There are already some conditions that return False.
     # TODO: might need to filter out more invalid extensions
-    # TODO: filter out urls not containing ics.uci.edu domain
     
     try:
         parsed = urlparse(url)
         if parsed.scheme not in set(["http", "https"]):
             return False
+
+        # TODO: transform relative to absolute urls
+
+
+        # FILTER OUT: non ics.uci.edu domains
+        domains = ["ics.uci.edu", "cs.uci.edu", "informatics.uci.edu", "stat.uci.edu"]
+        if not any(domain in parsed.netloc for domain in domains):
+            return False
+        # if it's already in the buffer.txt then we also skip
         return not re.match(
             r".*\.(css|js|bmp|gif|jpe?g|ico"
             + r"|png|tiff?|mid|mp2|mp3|mp4"
